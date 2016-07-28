a3177f8539c42505d74db7e4fca311d0352125e9
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-28 19:46:04 +0200

    version update

--- decimal2rational/__init__.py
+++ decimal2rational/__init__.py
@@ -5,7 +5,7 @@ __all__ = [
     'decimal2rational'
     ]
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__ = 'Nico Schlömer'
 __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/decimal2rational'
