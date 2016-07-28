88a10330584d82ab35e10d12a7a432214a017851
Nico Schlömer <nico.schloemer@gmail.com>
2016-07-28 20:40:11 +0200

    account for exp, logn

--- decimal2rational/__init__.py
+++ decimal2rational/__init__.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 #
-from math import pi
-
 __all__ = [
     'decimal2rational'
     ]
@@ -12,12 +10,24 @@ __author_email__ = 'nico.schloemer@gmail.com'
 __website__ = 'https://github.com/nschloe/decimal2rational'
 
 
-def decimal2rational(a, max_denominator=1000, max_root=5, tol=1.0e-10):
+def decimal2rational(a, max_denominator=1000, tol=1.0e-15):
+    from math import pi, exp, log
 
-    for mult_pi in range(2):
-        for root in range(1, max_root+1):
-            a0 = a
-            a0 **= root
+    funs = [
+        (lambda x: x, None),
+        (lambda x: x**2, 'sqrt'),
+        (lambda x: x**3, 'root3')
+        ]
+
+    if a > 0:
+        funs.append((lambda x: log(x), 'exp'))
+
+    funs.append((lambda x: exp(x), 'logn'))
+
+    for fun, fun_name in funs:
+        a0 = a
+        a0 = fun(a0)
+        for mult_pi in range(2):
             if mult_pi > 0:
                 a0 /= pi**mult_pi
 
@@ -25,10 +35,6 @@ def decimal2rational(a, max_denominator=1000, max_root=5, tol=1.0e-10):
                 rd = round(a0*den)
                 diff = a0*den - rd
                 if abs(diff) < tol * a0*den:
-                    num0 = int(rd)
-                    den0 = den
-                    root0 = root
-                    diff0 = diff
-                    return int(rd), den, mult_pi, root, diff
+                    return int(rd), den, mult_pi, fun_name, diff
 
     return None, None, None, None, None
--- test/test.py
+++ test/test.py
@@ -1,82 +1,109 @@
 # -*- coding: utf-8 -*-
 #
 import decimal2rational
-from numpy import pi
+from math import pi, exp, log
+
 
 def test_rational():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational(3.0 / 7.0)
 
     assert num == 3
     assert den == 7
     assert mult_pi == 0
-    assert root == 1
+    assert fun is None
 
     return
 
 
 def test_root2():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational((3.0 / 7.0)**0.5)
 
     assert num == 3
     assert den == 7
     assert mult_pi == 0
-    assert root == 2
+    assert fun == 'sqrt'
 
     return
 
 
 def test_root3():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational((3.0 / 7.0)**(1.0/3.0))
 
     assert num == 3
     assert den == 7
     assert mult_pi == 0
-    assert root == 3
+    assert fun == 'root3'
 
     return
 
 
 def test_root34():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational(3.0**0.5 / 2.0)
 
     assert num == 3
     assert den == 4
     assert mult_pi == 0
-    assert root == 2
+    assert fun == 'sqrt'
 
     return
 
 
 def test_pi():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational(3.0/4.0 * pi)
 
     assert num == 3
     assert den == 4
     assert mult_pi == 1
-    assert root == 1
+    assert fun is None
 
     return
 
 
 def test_sqrt_pi_2():
 
-    num, den, mult_pi, root, diff = \
+    num, den, mult_pi, fun, diff = \
         decimal2rational.decimal2rational((0.5*pi)**0.5)
 
     assert num == 1
     assert den == 2
     assert mult_pi == 1
-    assert root == 2
+    assert fun == 'sqrt'
+
+    return
+
+
+def test_exp_2_pi():
+
+    num, den, mult_pi, fun, diff = \
+        decimal2rational.decimal2rational(exp(2*pi))
+
+    assert num == 2
+    assert den == 1
+    assert mult_pi == 1
+    assert fun == 'exp'
+
+    return
+
+
+def test_logn_0_5():
+
+    num, den, mult_pi, fun, diff = \
+        decimal2rational.decimal2rational(log(0.5))
+
+    assert num == 1
+    assert den == 2
+    assert mult_pi == 0
+    assert fun == 'logn'
 
     return
 
