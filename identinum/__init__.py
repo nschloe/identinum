8ad9bcb7ad03eca33686b09de19bf7efafd7be83
Nico Schlömer <nico.schloemer@gmail.com>
2021-03-10 19:21:40 +0100

    modernization

--- identinum/__init__.py
+++ identinum/__init__.py
@@ -1,12 +1,9 @@
 from . import cli
-from .__about__ import __author__, __author_email__, __version__, __website__
+from .__about__ import __version__
 from .main import findpoly, identify
 
 __all__ = [
     "__version__",
-    "__author__",
-    "__author_email__",
-    "__website__",
     "identify",
     "findpoly",
     "cli",
